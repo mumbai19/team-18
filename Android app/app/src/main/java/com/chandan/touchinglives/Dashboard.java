fc5a385e6d98359a06c8f7574fe3d7fc5b1c3645
Chandan Bhangale <chandan.b1998@gmail.com>
2019-07-20 18:16:27 +0530

    Form added database entry testing

--- Android app/app/src/main/java/com/chandan/touchinglives/Dashboard.java
+++ Android app/app/src/main/java/com/chandan/touchinglives/Dashboard.java
@@ -1,20 +1,31 @@
 package com.chandan.touchinglives;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
-
+    private CardView AddActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+AddActivity=findViewById(R.id.addActivity);
+        AddActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Dashboard.this, AddActivity.class);
+                startActivity(i);
 
+
+            }
+        });
 
 
         }
--- Android app/app/src/main/java/com/chandan/touchinglives/MainActivity.java
+++ Android app/app/src/main/java/com/chandan/touchinglives/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chandan.touchinglives;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    
+
     }
 }
