31164d9828ae646eead8c3bba0590f5eae67f54b
Chandan Bhangale <chandan.b1998@gmail.com>
2019-07-20 19:53:27 +0530

    implementation added

--- Android app/app/src/main/java/com/chandan/touchinglives/AddActivity.java
+++ Android app/app/src/main/java/com/chandan/touchinglives/AddActivity.java
@@ -1,5 +1,6 @@
 package com.chandan.touchinglives;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -18,7 +20,17 @@ public class AddActivity extends AppCompatActivity {
         TextView etActName,etDescription;
         Button btnAddAct;
 
+etActName=findViewById(R.id.etActName);
+etDescription=findViewById(R.id.etDescription);
+btnAddAct=findViewById(R.id.btnAddAct);
 
+btnAddAct.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),"Activity record added successfully.", Toast.LENGTH_SHORT).show();
+
+    }
+});
 
 
     }
