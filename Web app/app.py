6ae32a7d9ad6953a91c190333d283cd7aec6e265
sarvesh12k26 <sarveshjoglekar11@gmail.com>
2019-07-20 18:12:51 +0530

    first-api

--- Web app/app.py
+++ Web app/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask import render_template,url_for,flash,redirect,request,abort,Flask,jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	return "Hi"
+
+@app.route("/program",methods=["GET"])
+def programname():
+	progname=["Beginner","Intermediate","Advanced"]
+	return jsonify({"key1":2})
