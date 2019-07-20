bd3b20ce237f7284429060094519b101591a7577
manoj-ochaney <manoj.ochaney@gmail.com>
2019-07-20 20:12:24 +0530

    Update app.py

--- Web app/app.py
+++ Web app/app.py
@@ -1,6 +1,22 @@
 from flask import Flask
+import pymysql
+
 app = Flask(__name__)
 
-@app.route("/")
+host = "127.0.0.1"
+user = "root"
+password = ""
+db = "test_cms"
+
+@app.route("/dept")
 def home():
-	return "Hi"
+
+	con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor)
+	cur = con.cursor()
+
+	cur.execute("SELECT * FROM department")
+	result = cur.fetchall()
+
+	return result[0]
+
+app.run()
