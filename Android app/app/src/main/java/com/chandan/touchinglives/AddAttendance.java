06532e145637f4a5d9dff1532f618ede000e04e3
Chandan Bhangale <chandan.b1998@gmail.com>
2019-07-21 04:15:08 +0530

    Attendence added

--- Android app/app/src/main/java/com/chandan/touchinglives/AddAttendance.java
+++ Android app/app/src/main/java/com/chandan/touchinglives/AddAttendance.java
@@ -5,7 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class AddAttendance extends AppCompatActivity {
 
@@ -13,7 +17,21 @@ public class AddAttendance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_attendance);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getApplicationContext(),R.layout.activity_add_remove_student,R.id.textView1,getResources().getStringArray(R.array.Strings));
+        setListAdapter(adapter);
 
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item+" selected", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_my_list_view_with_toggle, menu);
+        return true;
+    }
+}
 
         }
 
